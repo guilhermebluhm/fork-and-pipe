2c7dd8cdc00ca6b490bc83c5b405eb18aa4696e0
Guilherme Saraiva Bluhm <80696919+guilhermebluhm@users.noreply.github.com>
2023-04-22 11:01:43 -0300

    first commit

---
+++ processForkAndPipe.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(){
+
+    int fd[2];
+    /*
+    file descriptor
+    fd[0] -> read
+    fd[1] -> write
+    */
+
+   if(pipe(fd) == -1){
+        std::cout << "error when create pipe" << "\n";
+        return -1;
+   }
+
+   pid_t pid = fork();
+   if(pid < 0){
+        std::cout << "error when create fork()" << "\n";
+        return -1;
+   }
+
+   else{
+    //parent process
+    if(pid > 0){
+        //close write process
+        close(fd[1]);
+        int resp;
+        if(read(fd[0], &resp, sizeof(int)) == -1){
+            std::cout << "error while read process in pipe\n";
+            return -1;
+        }
+        std::cout << "i'm parent process" << "\n";
+        std::cout << "message for child process: " << resp << "\n";
+        //close read process
+        close(fd[0]);
+        wait(NULL);
+        std::cout << "finishing parent process";
+    }
+    //child process
+    if(pid == 0){
+        int message;
+        //close for read process
+        close(fd[0]);
+        std::cout << "i'm child process" << "\n";
+        std::cout << "input for parent process: ";
+        std::cin >> message;
+        if(write(fd[1], &message, sizeof(int)) == -1){
+            std::cout << "error while write process in pipe\n";
+            return -1;
+        }
+        //close write process
+        close(fd[1]);
+        std::cout << "finishing child process" << "\n";
+    }
+   }
+}
